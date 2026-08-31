33db0dca9ff9d7489475b70026e5435934c27e01
leonardoredivo-rgb <leonardo.redivo@escola.pr.gov.br>
2026-08-31 18:29:00 -0300

    bfxcz

---
+++ script.js
@@ -0,0 +1,11 @@
+/*like*/
+const botoes = document.querySelectorAll("button");
+
+botoes.forEach(function (botao){
+    botao.addEventListener("click", botaoclicado);
+    function botaoclicado(){
+    console.log("fui clicado");
+    let texto = botao.querySelector("span");
+    texto.textContent++;
+    }
+})
